43a76757afa194052ba7311654b09d7f5296450e
soham009 <sohamthaker17@gmail.com>
2020-03-13 14:27:13 +0530

    learning nodejs - File System Module  commit

--- node_module_system.js
+++ node_module_system.js
@@ -2,3 +2,6 @@
 const fs = require('fs')
 // Using the fs object we call the function writeFileSync to create a new File named as test.txt
 fs.writeFileSync('test.txt', 'I am Soham Thaker')
+
+//Using the fs object we call the append function appendFileSync to append the test.txt file
+fs.appendFileSync('test.txt', 'I am Soham Thaker and I am trying to Append this file')
