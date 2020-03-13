e9e7c9d5b26767b2bcd48e0f9b42ef98132384a4
soham009 <sohamthaker17@gmail.com>
2020-03-13 15:01:36 +0530

    Learning NodeJs - Importing from My Module

---
+++ importing_my_file.js
@@ -0,0 +1,8 @@
+// Importing Variables and Functions into my_module variable
+const my_module = require('./my_module.js')
+
+// Using function imported from my Imported Module
+console.log(my_module.add(1,5))
+
+// Using variable defined in My Imported Module
+console.log(my_module.name)
---
+++ my_module.js
@@ -0,0 +1,11 @@
+// Defining a variable in My Module
+const name = ' Soham Thaker'
+
+// Function to Add 2 number
+const add = function (a, b) {
+    return a + b
+}
+
+
+//Exporting Variable and Function from My Module
+module.exports = {name,add}
